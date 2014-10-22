3baf1a2df28e65649ac61c361ae8b59def091cdf
Mitchel Humpherys <mitch.special@gmail.com>
2014-10-22 00:16:46 -0700

    initial project creation

---
+++ firmware/main.c
@@ -0,0 +1,16 @@
+/* Name: main.c
+ * Author: Mitchel Humpherys
+ * Copyright: 2014
+ * License: Creative Commons
+ */
+
+#include <avr/io.h>
+
+int main(void)
+{
+    /* insert your hardware initialization here */
+    for(;;) {
+        /* insert your main loop code here */
+    }
+    return 0;   /* never reached */
+}
